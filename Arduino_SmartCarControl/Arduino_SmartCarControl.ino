153bc9b6407d28a017202f962e4fd867f035e388
Le Ly Lam <108975594+LeLyLam@users.noreply.github.com>
2024-08-12 18:53:50 +0700

    test 3

--- Arduino_SmartCarControl/Arduino_SmartCarControl.ino
+++ Arduino_SmartCarControl/Arduino_SmartCarControl.ino
@@ -4,126 +4,124 @@
 const int servoPin = 11;
 const int echoPin = 9;
 const int triggerPin = 10;
-const int maxDistance = 200;
-
 const int in1Pin = 8;
 const int in2Pin = 7;
 const int in3Pin = 6;
 const int in4Pin = 4;
 const int enaPin = 5;
 const int enbPin = 3;
-const int speed = 255;
-// Define servo angles to check
-const int leftAngle = 30;
-const int rightAngle = 150;
-const int centerAngle = 90;
-
+const int maxDistance = 200;
 Servo myServo;
 NewPing sonar(triggerPin, echoPin, maxDistance);
 
+//const int speed = 255;
+bool movingForward = false;
+
 void setup() {
+  Serial.begin(9600);
   myServo.attach(servoPin);
   pinMode(in1Pin, OUTPUT);
   pinMode(in2Pin, OUTPUT);
   pinMode(in3Pin, OUTPUT);
   pinMode(in4Pin, OUTPUT);
-  pinMode(enaPin, OUTPUT);
-  pinMode(enbPin, OUTPUT);
-  myServo.write(centerAngle);
+  // pinMode(enaPin, OUTPUT);
+  // pinMode(enbPin, OUTPUT);
+  pinMode(triggerPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  myServo.write(90);
   delay(1000);
 }
 
 void loop() {
-  if (checkObstacle()) {
-    chooseDirection();
+  /* test tránh vật cản
+  long distance = getDistance();
+  if (movingForward == true && distance < 20) {
+    stop();
+    long distanceRight = scanRight();
+    long distanceLeft = scanLeft();
+    if (distanceLeft > distanceRight) {
+      left();
+    } else {
+      right();
+    }
+    delay(500);
   } else {
-    moveForward();
+    forward();
+    delay(100);
   }
-  delay(100);
-}
-
-bool checkObstacle() {
-  unsigned int distance = sonar.ping_cm();
-  if (distance > 0 && distance < 20) {
-    return true;
-  }
-  return false;
-}
-
-void chooseDirection() {
-  stopMovement();
-  delay(500);
-
-  moveBackward();
+  */
+  /* test di chuyển */
+  forward();
   delay(1000);
-  stopMovement();
-  delay(500);
-
-  // Check left
-  myServo.write(leftAngle);
-  delay(500);
-  unsigned int leftDistance = sonar.ping_cm();
-  
-  // Check right
-  myServo.write(rightAngle);
-  delay(500);
-  unsigned int rightDistance = sonar.ping_cm();
-  
-  // Return to center position
-  myServo.write(centerAngle);
-  delay(500);
-  
-  // Choose direction based on distances
-  if (leftDistance > rightDistance) {
-    turnLeft();
-  } else {
-    turnRight();
-  }
+  backward();
   delay(1000);
-  moveForward();
+  left();
+  delay(1000);
+  right();
+  delay(1000);
+  stop();
+  delay(1000);
+
 }
 
-void moveForward() {
-  digitalWrite(in1Pin, LOW);
-  digitalWrite(in2Pin, HIGH);
-  analogWrite(enaPin, speed);
-
-  digitalWrite(in3Pin, LOW);
-  digitalWrite(in4Pin, HIGH);
-  analogWrite(enbPin, speed);
+long getDistance(){
+  return sonar.ping_cm();
 }
 
-void moveBackward() {
-  digitalWrite(in1Pin, HIGH);
+long scanRight() {
+  myServo.write(30);
+  delay(300);
+  long distance = getDistance();
+  myServo.write(90);
+  delay(300);
+  return distance;
+}
+
+long scanLeft() {
+  myServo.write(150);
+  delay(300);
+  long distance = getDistance();
+  myServo.write(90);
+  delay(300);
+  return distance;
+}
+
+void forward() {
+  movingForward = true;
   digitalWrite(in2Pin, LOW);
-  analogWrite(enaPin, speed);
-
-  digitalWrite(in3Pin, HIGH);
   digitalWrite(in4Pin, LOW);
-  analogWrite(enbPin, speed);
-}
-
-void turnLeft() {
-  digitalWrite(in1Pin, LOW);
-  digitalWrite(in2Pin, HIGH);
-  analogWrite(enaPin, speed);
-
-  digitalWrite(in3Pin, HIGH);
-  digitalWrite(in4Pin, LOW);
-  analogWrite(enbPin, speed);
-}
-
-void turnRight() {
   digitalWrite(in1Pin, HIGH);
-  digitalWrite(in2Pin, LOW);
-  analogWrite(enaPin, speed);
-
-  digitalWrite(in3Pin, LOW);
-  digitalWrite(in4Pin, HIGH);
-  analogWrite(enbPin, speed);
+  digitalWrite(in3Pin, HIGH);
 }
 
-void stopMovement() {
-  analogWrite(enaPin, 0);
-  analogWrite(enbPin, 0);
+void backward() {
+  movingForward = false;
+  digitalWrite(in1Pin, LOW);
+  digitalWrite(in3Pin, LOW);
+  digitalWrite(in2Pin, HIGH);
+  digitalWrite(in4Pin, HIGH);
+}
+
+void right() {
+  movingForward = true;
+  digitalWrite(in2Pin, LOW);
+  digitalWrite(in3Pin, LOW);
+  digitalWrite(in4Pin, LOW);
+  digitalWrite(in1Pin, HIGH);
+}
+
+void left() {
+  movingForward = true;
+  digitalWrite(in1Pin, LOW);
+  digitalWrite(in2Pin, LOW);
+  digitalWrite(in4Pin, LOW);
+  digitalWrite(in3Pin, HIGH);
+}
+
+void stop() {
+  movingForward = false;
+  digitalWrite(in1Pin, LOW);
+  digitalWrite(in2Pin, LOW);
+  digitalWrite(in3Pin, LOW);
+  digitalWrite(in4Pin, LOW);
 }
